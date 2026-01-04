8dcb4ff67aa7b002a1bcbbafc32ad1bbe463af21
Jakobi Haskell <jakobihaskell@gmail.com>
2026-01-03 16:01:42 -0800

    fix weird gap between left and right side

--- src/components/layout/main-layout.tsx
+++ src/components/layout/main-layout.tsx
@@ -19,7 +19,7 @@ export const MainLayout = ({ children }: MainLayoutProps) => {
                 footerItems={FOOTER_NAV_ITEMS}
                 activeUrl={pathname}
             />
-            <main className="flex-1 lg:pl-[296px]">
+            <main className="flex-1">
                 <div className="mx-auto max-w-7xl px-4 py-8 sm:px-6 lg:px-8">
                     {children}
                 </div>
